e7d15ba792798873604eae20959e25c7ad67ad0d
BIT <BIT@DESKTOP-4JVMJE1>
2019-05-07 15:37:32 +0900

    조인까지

--- practice.sql
+++ practice.sql
@@ -61,7 +61,7 @@ select * From titles where emp_no = 13250 order by from_date;
 -- 최고임금(salary)과  최저임금을 “최고임금, “최저임금”프로젝션 타이틀로 함께 출력해 보세요. 
 -- 두 임금의 차이는 얼마인가요? 함께 “최고임금 – 최저임금”이란 타이틀로 출력해 보세요.
 select max(salary) as "최고임금",
-		min(salary) as "최저임금",
+      min(salary) as "최저임금",
         max(salary)-min(salary) as "최고임금 - 최저임금"
 from salaries;
 
@@ -97,19 +97,19 @@ from employees;
 -- 문제 1. 
 -- 현재 급여가 많은 직원부터 직원의 사번, 이름, 그리고 연봉을 출력 하시오.
 select distinct e.emp_no as "사번",
-		concat(first_name,' ',last_name) as "이름",
+      concat(first_name,' ',last_name) as "이름",
         (select max(salary) from salaries where emp_no=e.emp_no) as "연봉"
  from employees e inner join salaries s
-					on e.emp_no = s.emp_no;
+               on e.emp_no = s.emp_no;
                     
 
 -- 문제2.
 -- 전체 사원의 사번, 이름, 현재 직책을 이름 순서로 출력하세요.
 select e.emp_no as "사번",
-		concat(first_name,' ',last_name) as "이름",
+      concat(first_name,' ',last_name) as "이름",
         (select title from titles where now() between from_date and to_date and emp_no = e.emp_no) as "현재 직책"
  From employees e inner join titles t 
-					on e.emp_no = t.emp_no
+               on e.emp_no = t.emp_no
                     order by first_name
                     ;
 
@@ -120,8 +120,8 @@ e.emp_no as "사번",
 concat(first_name,' ',last_name) as "이름",
 d.dept_name as "현재 부서"        
  from employees e inner join dept_emp de
-						on e.emp_no = de.emp_no inner join departments d
-							on de.dept_no = d.dept_no
+                  on e.emp_no = de.emp_no inner join departments d
+                     on de.dept_no = d.dept_no
                             where now() between from_date and to_date
                              order by first_name;
                             
@@ -138,12 +138,12 @@ t.title as "직책",
 d.dept_name as "부서"
 from
 employees e inner join dept_emp de
-	on e.emp_no = de.emp_no inner join departments d
-		on de.dept_no = d.dept_no inner join salaries s 
-			on e.emp_no = s.emp_no inner join titles t
-				on e.emp_no = t.emp_no
+   on e.emp_no = de.emp_no inner join departments d
+      on de.dept_no = d.dept_no inner join salaries s 
+         on e.emp_no = s.emp_no inner join titles t
+            on e.emp_no = t.emp_no
                 where now() between de.from_date and de.to_date and
-					now() between s.from_date and s.to_date and
+               now() between s.from_date and s.to_date and
                     now() between t.from_date and t.to_date
                     order by first_name;
 
@@ -155,8 +155,8 @@ select
 distinct e.emp_no, 
 concat(first_name,' ',last_name) as "이름"
 from employees e inner join titles t
-				on e.emp_no = t.emp_no
-					where t.title = 'Technique Leader' and  not (now() between t.from_date and t.to_date);
+            on e.emp_no = t.emp_no
+               where t.title = 'Technique Leader' and  not (now() between t.from_date and t.to_date);
 
 -- 문제6.
 -- 직원 이름(last_name) 중에서 S(대문자)로 시작하는 직원들의 이름, 부서명, 직책을 조회하세요.
@@ -165,31 +165,49 @@ concat(first_name,' ',last_name) as "이름",
 d.dept_name as "부서",
 t.title as "직책"
  from employees e inner join titles t on e.emp_no = t.emp_no
-							inner join dept_emp de on e.emp_no = de.emp_no
-								inner join departments d on de.dept_no = d.dept_no
+                     inner join dept_emp de on e.emp_no = de.emp_no
+                        inner join departments d on de.dept_no = d.dept_no
                                 where e.last_name like 'S%'
-									and now() between t.from_date and t.to_date
-										and now() between de.from_date and de.to_date;
+                           and now() between t.from_date and t.to_date
+                              and now() between de.from_date and de.to_date;
 
 -- 문제7.
 -- 현재, 직책이 Engineer인 사원 중에서 현재 급여가 40000 이상인 사원을 급여가 큰 순서대로 출력하세요.
 select * from
 employees e inner join titles t on e.emp_no = t.emp_no
-	inner join salaries s on e.emp_no = s.emp_no
-		where now() between s.from_date and s.to_date
-				and s.salary >= 40000;
+   inner join salaries s on e.emp_no = s.emp_no
+      where now() between s.from_date and s.to_date
+            and s.salary >= 40000;
 
 
 -- 문제8.
 -- 현재 급여가 50000이 넘는 직책을 직책, 급여로 급여가 큰 순서대로 출력하시오
-
+select t.title, s.salary from employees e inner join salaries s 
+                     on e.emp_no = s.emp_no inner join titles t
+                        on t.emp_no = e.emp_no 
+                                where now() between s.from_date and s.to_date
+                           and now() between t.from_date and t.to_date
+                              and s.salary > 50000 order by s.salary desc;
+                           
 
 -- 문제9.
 -- 현재, 부서별 평균 연봉을 연봉이 큰 부서 순서대로 출력하세요.
+select d.dept_name as "부서",avg(s.salary) as "평균연봉" from employees e inner join salaries s
+         on e.emp_no = s.emp_no inner join dept_emp de
+            on e.emp_no = de.emp_no inner join departments d
+               on d.dept_no = de.dept_no
+                    where now() between s.from_date and s.to_date
+                    group by d.dept_name order by avg(s.salary) desc;
+
+
 
 -- 문제10.
 -- 현재, 직책별 평균 연봉을 연봉이 큰 직책 순서대로 출력하세요.
-
+select t.title as "직책",avg(s.salary) as "평균 연봉" from employees e inner join salaries s 
+         on e.emp_no = s.emp_no inner join titles t   
+            on e.emp_no = t.emp_no 
+               where now() between s.from_date and s.to_date
+                  group by t.title order by avg(s.salary) desc; 
 
 ---------------------------------------------------------------------------------------
 
@@ -197,27 +215,55 @@ employees e inner join titles t on e.emp_no = t.emp_no
 
 -- 문제1.
 -- 현재 평균 연봉보다 많은 월급을 받는 직원은 몇 명이나 있습니까?
+select count(*) from salaries where salary > (select avg(salary) from salaries where now() between from_date and to_date)
+                        and now() between from_date and to_date;
 
 -- 문제2. 
--- 현재, 각 부서별로 최고의 급여를 받는 사원의 사번, 이름, 부서 연봉을 조회하세요. 단 조회결과는 연봉의 내림차순으로 정렬되어 나타나야 합니다. 
+-- 현재, 각 부서별로 최고의 급여를 받는 사원의 사번, 이름, 부서 연봉을 조회하세요. 
+-- 단 조회결과는 연봉의 내림차순으로 정렬되어 나타나야 합니다. -----
 
--- 문제3.
+-- 문제3. --
 -- 현재, 자신의 부서 평균 급여보다 연봉(salary)이 많은 사원의 사번, 이름과 연봉을 조회하세요 
+
 
 -- 문제4.
 -- 현재, 사원들의 사번, 이름, 매니저 이름, 부서 이름으로 출력해 보세요.
+select 
+e.emp_no as "사번",
+e.first_name as "이름",
+(select first_name from employees where emp_no = dm.emp_no) as "매니저 이름",
+d.dept_name as "부서 이름"
+ from employees e inner join dept_emp de 
+						on e.emp_no = de.emp_no inner join departments d
+							on d.dept_no = de.dept_no inner join dept_manager dm
+								on d.dept_no = dm.dept_no;
 
 -- 문제5.
 -- 현재, 평균연봉이 가장 높은 부서의 사원들의 사번, 이름, 직책, 연봉을 조회하고 연봉 순으로 출력하세요.
+select d.dept_no, avg(salary) from employees e inner join salaries s
+				on e.emp_no = s.emp_no inner join dept_emp de
+					on de.emp_no = e.emp_no inner join departments d
+						on d.dept_no = de.dept_no
+group by d.dept_no order by avg(salary) desc limit 1;
 
--- 문제6.
+
+
+
+-- 문제6. --
 -- 평균 연봉이 가장 높은 부서는? 
+select * from employees e inner join salaries s 
+					on e.emp_no = s.emp_no inner join dept_emp de 
+						on de.emp_no = e.emp_no inner join departments d	
+							on de.dept_no = d.dept_no;
+
 
 -- 문제7.
 -- 평균 연봉이 가장 높은 직책?
+select title,avg(salary) from employees e inner join salaries s 
+					on e.emp_no = s.emp_no inner join titles t
+						on e.emp_no = t.emp_no group by title order by avg(salary) desc limit 1;
 
 -- 문제8.
 -- 현재 자신의 매니저보다 높은 연봉을 받고 있는 직원은?
 -- 부서이름, 사원이름, 연봉, 매니저 이름, 메니저 연봉 순으로 출력합니다.
-
 
