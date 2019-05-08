a383b9173d508e0845e917dd857cfe3f4ef53138
BIT <BIT@DESKTOP-4JVMJE1>
2019-05-08 16:33:06 +0900

    서브쿼리 완료

--- practice04.sql
+++ practice04.sql
@@ -13,23 +13,35 @@ count(*) as "현재 평균연봉보다 많은 월급을 받는 직원 수"
 -- 현재, 각 부서별로 최고의 급여를 받는 사원의 사번, 이름, 부서 연봉을 조회하세요. 
 -- 단 조회결과는 연봉의 내림차순으로 정렬되어 나타나야 합니다.  o
 
-select 
-d.dept_name,
-e.emp_no,
-concat(e.last_name," ",e.first_name) as "이름",
-s.salary
-from employees e inner join salaries s
-		on e.emp_no = s.emp_no inner join dept_emp de
-			on de.emp_no = e.emp_no inner join departments d
-				on d.dept_no = de.dept_no
-where (d.dept_no,s.salary) in (
-	select departments.dept_no,max(salaries.salary) from employees inner join salaries
-			on employees.emp_no = salaries.emp_no inner join dept_emp
-				on employees.emp_no = dept_emp.emp_no inner join departments
-					on departments.dept_no = dept_emp.dept_no
-                    where salaries.to_date = '9999-01-01'
-                    group by departments.dept_no
-) and s.to_date = '9999-01-01' order by s.salary desc;
+SELECT 
+    d.dept_name,
+    e.emp_no,
+    CONCAT(e.last_name, ' ', e.first_name) AS '이름',
+    s.salary
+FROM
+    employees e
+        INNER JOIN
+    salaries s ON e.emp_no = s.emp_no
+        INNER JOIN
+    dept_emp de ON de.emp_no = e.emp_no
+        INNER JOIN
+    departments d ON d.dept_no = de.dept_no
+WHERE
+    (d.dept_no , s.salary) IN (SELECT 
+            departments.dept_no, MAX(salaries.salary)
+        FROM
+            employees
+                INNER JOIN
+            salaries ON employees.emp_no = salaries.emp_no
+                INNER JOIN
+            dept_emp ON employees.emp_no = dept_emp.emp_no
+                INNER JOIN
+            departments ON departments.dept_no = dept_emp.dept_no
+        WHERE
+            salaries.to_date = '9999-01-01'
+        GROUP BY departments.dept_no)
+        AND s.to_date = '9999-01-01'
+ORDER BY s.salary DESC;
 
 
 -- 문제3. --
@@ -59,20 +71,19 @@ s.salary
                 and s.to_date = '9999-01-01'
                 and d.to_date = '9999-01-01'
                 and s.salary > (
-				
-                select a.avg_salary from
-					(select 
-						de.dept_no as departmentno,
-						avg(s.salary) as avg_salary
-							from employees e, salaries s, dept_emp de
-								where e.emp_no = s.emp_no 
-									and e.emp_no = de.emp_no
-									and s.to_date = '9999-01-01'
-									and de.to_date = '9999-01-01'
-										group by de.dept_no) a
-											where a.departmentno = d.dept_no
+					select a.avg_salary from
+						(select 
+							de.dept_no as departmentno,
+							avg(s.salary) as avg_salary
+								from employees e, salaries s, dept_emp de
+									where e.emp_no = s.emp_no 
+										and e.emp_no = de.emp_no
+										and s.to_date = '9999-01-01'
+										and de.to_date = '9999-01-01'
+											group by de.dept_no) a
+												where a.departmentno = d.dept_no
                 
-);
+				);
 
 
 
@@ -138,12 +149,19 @@ group by d.dept_no order by avg(salary) desc limit 1;
 
 -- 문제6. --
 -- 평균 연봉이 가장 높은 부서는?  o 
-select d.dept_no,d.dept_name, avg(s.salary) from employees e inner join salaries s 
-					on e.emp_no = s.emp_no inner join dept_emp de 
-						on de.emp_no = e.emp_no inner join departments d	
-							on de.dept_no = d.dept_no
-                            group by d.dept_no, d.dept_name
-                            order by avg(s.salary) desc limit 1;
+SELECT 
+    d.dept_no, d.dept_name, AVG(s.salary)
+FROM
+    employees e
+        INNER JOIN
+    salaries s ON e.emp_no = s.emp_no
+        INNER JOIN
+    dept_emp de ON de.emp_no = e.emp_no
+        INNER JOIN
+    departments d ON de.dept_no = d.dept_no
+GROUP BY d.dept_no , d.dept_name
+ORDER BY AVG(s.salary) DESC
+LIMIT 1;
 
 
 -- 문제7.
@@ -171,42 +189,51 @@ group by dm.dept_no) a
 where a.dept_no = 'd001';
 
 -- 8번답??
-select 
-d.dept_name,
-e.first_name,
-s.salary,
-(
-select avg_salary from
-(select 
-dm.dept_no,
-avg(s.salary) as avg_salary
- from employees e, salaries s, dept_manager dm
-			where e.emp_no = s.emp_no 
-				and e.emp_no = dm.emp_no
-                and s.to_date = '9999-01-01'
-                and dm.to_date = '9999-01-01'
-group by dm.dept_no) a
-where a.dept_no = de.dept_no
-) as "매니저 연봉"
- from employees e, salaries s,dept_emp de,departments d
-			where e.emp_no = s.emp_no
-				and e.emp_no = de.emp_no
-                and de.dept_no = d.dept_no
-                and s.to_date = '9999-01-01'
-                and de.to_date = '9999-01-01'
-                and s.salary > (
-					select avg_salary from
-(select 
-dm.dept_no,
-avg(s.salary) as avg_salary
- from employees e, salaries s, dept_manager dm
-			where e.emp_no = s.emp_no 
-				and e.emp_no = dm.emp_no
-                and s.to_date = '9999-01-01'
-                and dm.to_date = '9999-01-01'
-group by dm.dept_no) a
-where a.dept_no = de.dept_no
-			);
+SELECT 
+    d.dept_name,
+    e.first_name,
+    s.salary,
+    (SELECT 
+            avg_salary
+        FROM
+            (SELECT 
+                dm.dept_no, AVG(s.salary) AS avg_salary
+            FROM
+                employees e, salaries s, dept_manager dm
+            WHERE
+                e.emp_no = s.emp_no
+                    AND e.emp_no = dm.emp_no
+                    AND s.to_date = '9999-01-01'
+                    AND dm.to_date = '9999-01-01'
+            GROUP BY dm.dept_no) a
+        WHERE
+            a.dept_no = de.dept_no) AS '매니저 연봉'
+FROM
+    employees e,
+    salaries s,
+    dept_emp de,
+    departments d
+WHERE
+    e.emp_no = s.emp_no
+        AND e.emp_no = de.emp_no
+        AND de.dept_no = d.dept_no
+        AND s.to_date = '9999-01-01'
+        AND de.to_date = '9999-01-01'
+        AND s.salary > (SELECT 
+            avg_salary
+        FROM
+            (SELECT 
+                dm.dept_no, AVG(s.salary) AS avg_salary
+            FROM
+                employees e, salaries s, dept_manager dm
+            WHERE
+                e.emp_no = s.emp_no
+                    AND e.emp_no = dm.emp_no
+                    AND s.to_date = '9999-01-01'
+                    AND dm.to_date = '9999-01-01'
+            GROUP BY dm.dept_no) a
+        WHERE
+            a.dept_no = de.dept_no);
 
 
 
